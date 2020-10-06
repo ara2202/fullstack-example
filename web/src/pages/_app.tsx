2db4c2ce714f513a6c26bbd35d89244528c6b89b
Andrey Reutov <an.reutov@corp.mail.ru>
2020-10-06 22:19:06 +0300

    add custom query update on

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,13 +1,78 @@
 import { ThemeProvider, CSSReset, ColorModeProvider } from "@chakra-ui/core";
-import { Provider, createClient } from "urql";
+import { Provider, createClient, dedupExchange, fetchExchange } from "urql";
+import { cacheExchange, Cache, QueryInput } from "@urql/exchange-graphcache";
 
 import theme from "../theme";
+import {
+  LoginMutation,
+  MeDocument,
+  MeQuery,
+  RegisterMutation,
+} from "../generated/graphql";
+
+/* 
+  запрос me() кешируется, поэтому после логина мы не видим текущего юзера, т.к. запрос me был до логина и он закешировался
+  чтобы это обойти, мы устанавливаем доп. либу @urql/exchange-graphcache
+  и прописываем как проапдейтить запрос
+  Но поскольку там беда с типами, мы также пишем вспомогательную функцию betterUpdateQuery
+*/
+
+function betterUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
 
 const client = createClient({
   url: "http://localhost:4000/graphql",
   fetchOptions: {
     credentials: "include",
   },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, _, cache, __) => {
+            betterUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.login.user,
+                  };
+                }
+              }
+            );
+          },
+          register: (_result, _, cache, __) => {
+            betterUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.register.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
 
 function MyApp({ Component, pageProps }) {
