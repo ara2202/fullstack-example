2f9f331438a6166155e17b32e860f4ce635b042f
Andrey Reutov <an.reutov@corp.mail.ru>
2020-10-30 18:30:51 +0300

    fixed vote errors on backend

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -16,6 +16,7 @@ import {
 } from "type-graphql";
 import { Post } from "../entities/Post";
 import { getConnection } from "typeorm";
+import { UpDoot } from "../entities/Updoot";
 
 @InputType()
 class PostInput {
@@ -50,8 +51,59 @@ export class PostResolver {
     @Ctx() { req }: MyContext
   ) {
     const { userId } = req.session;
+
     const isUpdoot = value !== -1;
     const realValue = isUpdoot ? 1 : -1;
+
+    const userVote = await UpDoot.findOne({
+      where: {
+        postId,
+        userId,
+      },
+    });
+
+    if (userVote?.value !== realValue) {
+      // user has voted, but changed his mind
+      await getConnection().transaction(async (tm) => {
+        await tm.query(
+          `
+        update up_doot
+        set value = $1
+        where "postId" = $2 and "userId" = $3
+        `,
+          [realValue, postId, userId]
+        );
+
+        await tm.query(
+          `
+        update post
+        set points = points + $1
+        where id = $2;
+        `,
+          [2 * realValue, postId]
+        );
+      });
+    } else if (!userVote) {
+      // user has never voted before on this post
+      await getConnection().transaction(async (tm) => {
+        await tm.query(
+          `
+        insert into up_doot ("userId", "postId", value)
+        values ($1, $2, $3);
+        `,
+          [userId, postId, realValue]
+        );
+
+        await tm.query(
+          `
+        update post
+        set points = points + $1
+        where id = $2;
+        `,
+          [realValue, postId]
+        );
+      });
+    }
     // await UpDoot.insert({
     //   userId,
     //   postId,
@@ -59,20 +111,20 @@ export class PostResolver {
     // });
     // Выполняем SQL-транзакцию, чтобы либо внести изменения в обе таблицы
     // либо откатить все
-    await getConnection().query(
-      `
-      START TRANSACTION;
+    // await getConnection().query(
+    //   `
+    //   START TRANSACTION;
 
-      insert into up_doot ("userId", "postId", value)
-      values (${userId},${postId},${realValue});
+    //   insert into up_doot ("userId", "postId", value)
+    //   values (${userId},${postId},${realValue});
 
-      update post
-      set points = points + ${realValue}
-      where id = ${postId};
+    //   update post
+    //   set points = points + ${realValue}
+    //   where id = ${postId};
 
-      COMMIT;
-    `
-    );
+    //   COMMIT;
+    // `
+    // );
     return true;
   }
 
